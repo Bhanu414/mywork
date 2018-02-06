445b7e7ea4c37f3015bd5d2c11a12e50728b5509
BhanuBikki <bikki.usapp@gmail.com>
2018-02-05 19:35:44 -0500

    renaming folder

--- react-project/src/App.js
+++ react-project/src/App.js
@@ -47,7 +47,7 @@ class App extends Component {
       font:'inherit',
       border:'1px solid blue',
       padding: '8px',
-      curson: 'pointer'
+      cursor: 'pointer'
     }
     return (
       <div className="App">
